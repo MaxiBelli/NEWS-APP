f48145d11ba938fc7d7b71ce583f8b0de444192e
MaxiBelli <maximiliano_belli@hotmail.com>
2023-05-20 16:54:52 -0300

    feat: Add category detection and API news retrieval

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -6,22 +6,28 @@ import {
   Button,
   Box,
 } from "@mui/material";
-
+import useNews from "../hooks/useNews";
 import { CATEGORIES } from "../data";
 
 const Form = () => {
+  const { category, handleChangeCategory } = useNews();
+
   return (
     <form>
       <FormControl fullWidth>
         <InputLabel>Category</InputLabel>
-        <Select label="Category">
+        <Select
+          label="Category"
+          onChange={handleChangeCategory}
+          value={category}
+        >
           {CATEGORIES.map((category) => (
             <MenuItem key={category.value} value={category.value}>
               {category.label}
             </MenuItem>
           ))}
         </Select>
-        <Box sx={{marginTop: 2}}>
+        <Box sx={{ marginTop: 2 }}>
           <Button fullWidth variant="contained" color="primary">
             {" "}
             News Search
--- src/context/NewsProvider.jsx
+++ src/context/NewsProvider.jsx
@@ -1,9 +1,38 @@
+import axios from "axios";
 import { useState, useEffect, createContext } from "react";
 
 const NewsContext = createContext();
 
 const NewsProvider = ({ children }) => {
-  return <NewsContext.Provider value={{}}>{children}</NewsContext.Provider>;
+  const [category, setCategory] = useState("general");
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    const fetchAPI = async () => {
+      const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${category}&apiKey=${
+        import.meta.env.VITE_API_KEY
+      }`;
+
+      const { data } = await axios(url);
+      setNews(data.articles);
+    };
+    fetchAPI();
+  }, [category]);
+
+  const handleChangeCategory = (e) => {
+    setCategory(e.target.value);
+  };
+
+  return (
+    <NewsContext.Provider
+      value={{
+        category,
+        handleChangeCategory,
+      }}
+    >
+      {children}
+    </NewsContext.Provider>
+  );
 };
 
 export { NewsProvider };
